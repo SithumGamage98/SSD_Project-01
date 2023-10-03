3d034b92e78c086d85202c4b3ec73726bbf79b85
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-10-03 08:03:41 +0530

    Added backend model files

---
+++ Server/models/productModel.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+//Review Scheama
+const reviewSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    comment: { type: String, required: true },
+    rating: { type: Number, required: true },
+  },
+  {
+    timestamps: true, //Store crated date and updated date
+  }
+);
+
+//Product Scheams
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    countInStock: { type: Number, required: true },
+    brand: { type: String, required: true },
+    rating: { type: Number, required: true },
+    numReviews: { type: Number, required: true },
+    description: { type: String, required: true },
+    reviews: [reviewSchema],
+  },
+  {
+    timestamps: true, //Store crated date and updated date
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
